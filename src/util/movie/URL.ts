9c2495a30cfd203c2c961c1bd29b09b7b76ddd4f
jesuszero <djawndud5923@naver.com>
2024-11-21 17:40:01 +0900

    백업본

--- src/util/movie/URL.ts
+++ src/util/movie/URL.ts
@@ -22,5 +22,7 @@ export class URLService {
   getURL4GenreMovies = (apiKey: string, genre: string, page: number = 1) => {
     return `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genre}&language=ko-KR&page=${page}`;
   }
+  getURL4NowPlayingMovies(apiKey) {
+    return `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=ko-KR&page=1`;
+  }
 }
-
