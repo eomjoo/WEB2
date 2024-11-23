0085656d1934fcb13a7314fe8ca946f50bba7a54
jesuszero <djawndud5923@naver.com>
2024-11-24 00:37:40 +0900

    배포 오류 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.js';
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error('Root element not found. Ensure your index.html has a <div id="root"></div>.');
+}
+
+const root = createRoot(rootElement);
+root.render(
+  <React.StrictMode>
     <App />
-)
+  </React.StrictMode>
+);
