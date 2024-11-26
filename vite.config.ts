70af22af6fc987404cfef8b16c5a80991b7e9075
jesuszero <djawndud5923@naver.com>
2024-11-26 13:54:55 +0900

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   plugins: [react()],
-  base: '/WEB2/', // 배포 시의 base 경로
+  base: "/WEB2/", // 배포 시의 base 경로
   build: {
     outDir: 'dist',
   },
