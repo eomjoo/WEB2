c45250b622a4e9eff5ec734e6b1afa58f675f035
eomjoo <djawndud5923@naver.com>
2024-11-26 06:14:04 +0000

    build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(() => ({
   plugins: [react()],
   base: "/WEB2/", // 배포 시의 base 경로
-  build: {
-    outDir: 'dist',
-  },
 }));
